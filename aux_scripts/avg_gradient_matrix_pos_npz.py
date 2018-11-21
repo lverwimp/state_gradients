a8899674bf7ffcba60e2b843793e739ce308b196
lverwimp <lyan.verwimp@telenet.be>
2018-11-21 13:12:14 +0100

    Create avg_gradient_matrix_pos_npz.py

---
+++ aux_scripts/avg_gradient_matrix_pos_npz.py
@@ -0,0 +1,44 @@
+#! /usr/bin/env python
+# calculate average gradient for a certain delay
+# and for a specific class
+# for compressed gradient matrices
+
+import sys, glob, os, re
+import numpy as np
+from utils import read_pos_tags
+
+grad_dir = sys.argv[1]
+delay = int(sys.argv[2])
+pos_classes = sys.argv[3]
+pos_f = sys.argv[4] # file containing the mapping from words to POS
+
+classes = [c.strip() for c in open(pos_classes).readlines()]
+pos_mapping, _ = read_pos_tags(pos_f)
+
+list_grads = glob.glob('{0}/timestep_*/delay_{1}/*.npz'.format(grad_dir, delay))
+
+tmp_sum = 0.0
+tmp_denom = 0.0
+for grad_matrix in list_grads:
+
+    mapping_f = os.path.join(os.path.dirname(grad_matrix), 'mapping.txt')
+    words = []
+    for line in open(mapping_f, 'r'):
+        words.append(line.split()[1])
+
+    for i, w in enumerate(words):
+        if w in pos_mapping:
+            pos_curr = pos_mapping[w]
+
+            # check if the word is a class that we want to average over
+            for p in pos_curr:
+                if p in classes:
+                    tmp_sum += np.load(grad_matrix)['arr_0'][i]
+                    tmp_denom += 1
+                    break
+        else:
+            print('{0} not in pos_mapping'.format(w))
+
+avg = tmp_sum / tmp_denom
+
+np.save(os.path.join(grad_dir, 'delay_{0}_avg_{1}.npy'.format(delay, os.path.basename(pos_classes))), avg)
