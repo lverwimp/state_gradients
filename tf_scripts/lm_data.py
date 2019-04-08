153a66249bc2f91cee9f315c7cf67310b179f3d8
lverwimp <lyan.verwimp@telenet.be>
2019-04-08 14:47:47 +0200

    Update lm_data.py

--- tf_scripts/lm_data.py
+++ tf_scripts/lm_data.py
@@ -226,7 +226,7 @@ class LMData(object):
 
 			# save the item_to_id mapping such that it can be re-used
 			if 'save_dict' in self.config:
-				save_item_to_id(self.item_to_id, '{0}.dict'.format(self.config['save_dict']), self.encoding)
+				save_item_to_id(self.item_to_id, self.config['save_dict'], self.encoding)
 
 		# make a label file to visualize the embeddings
 		# with the correct labels (= words instead of ids) in tensorboard
